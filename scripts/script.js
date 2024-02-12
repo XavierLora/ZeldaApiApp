380de576a816fbf8c7c288f07511536974bdb0c7
Xavier Lora <XavierLora0727@gmail.com>
2024-02-12 16:04:16 -0500

    Functionality Updates

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,99 @@
-const testRequest = async () => {
-    const response = await fetch('https://botw-compendium.herokuapp.com/api/v3/compendium/entry/white-maned_lynel');
-    const myJson = await response.json();
-    const responseTest = await fetch('https://botw-compendium.herokuapp.com/api/v3/compendium/category/monsters');
-    const myJsonTest = await responseTest.json();
-    console.log(myJsonTest); 
-    console.log(myJsonTest.data.length);
-    console.log(myJson);
-    displayCard(myJson);
-    for(var i = 0; i < myJsonTest.data.length; i++){
-        var obj = myJsonTest.data[i];
-        displayTypeCard(obj);
-    }
+const CreatureCheckBox = document.getElementById("creatureCheck");
+const EquipmentCheckBox = document.getElementById("equipmentCheck");
+const MaterialCheckBox = document.getElementById("materialCheck");
+const MonsterCheckBox = document.getElementById("monsterCheck");
+
+function uncheckAllCheckboxes() {
+    CreatureCheckBox.checked = false;
+    EquipmentCheckBox.checked = false;
+    MaterialCheckBox.checked = false;
+    MonsterCheckBox.checked = false;
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    uncheckAllCheckboxes();
+});
+
+CreatureCheckBox.addEventListener('change', function(){
+    if (this.checked) { // Check if the checkbox is checked
+        fadeMain();
+        const creatureRequest = async () => {
+            const data = await fetch('https://botw-compendium.herokuapp.com/api/v3/compendium/category/creatures');
+            const myJsonData = await data.json();
+            console.log(myJsonData);
+            for(var i = 0; i < myJsonData.data.length; i++){
+                var obj = myJsonData.data[i];
+                displayTypeCard(obj);
+            }
+            document.getElementById('main').scrollTop = 0;
+        };
+        creatureRequest();
+    }else{
+        fadeMain();
+        removeCardsByCategory("creatures");
+        console.log("removeCards called");
+    }
+});
+
+EquipmentCheckBox.addEventListener('change', function(){
+    if (this.checked) { // Check if the checkbox is checked
+        fadeMain();
+        const equipmentRequest = async () => {
+            const data = await fetch('https://botw-compendium.herokuapp.com/api/v3/compendium/category/equipment');
+            const myJsonData = await data.json();
+            for(var i = 0; i < myJsonData.data.length; i++){
+                var obj = myJsonData.data[i];
+                displayTypeCard(obj);
+            }
+            document.getElementById('main').scrollTop = 0;
+        };
+        equipmentRequest();
+    }else{
+        fadeMain();
+        removeCardsByCategory("equipment");
+        console.log("removeCards called");
+    }
+});
+
+MaterialCheckBox.addEventListener('change', function(){
+    if (this.checked) { // Check if the checkbox is checked
+        fadeMain();
+        const equipmentRequest = async () => {
+            const data = await fetch('https://botw-compendium.herokuapp.com/api/v3/compendium/category/materials');
+            const myJsonData = await data.json();
+            for(var i = 0; i < myJsonData.data.length; i++){
+                var obj = myJsonData.data[i];
+                displayTypeCard(obj);
+            }
+            document.getElementById('main').scrollTop = 0;
+        };
+        equipmentRequest();
+    }else{
+        fadeMain();
+        removeCardsByCategory("materials");
+    }
+});
+
+MonsterCheckBox.addEventListener('change', function(){
+    if (this.checked) { // Check if the checkbox is checked
+        fadeMain();
+        const equipmentRequest = async () => {
+            const data = await fetch('https://botw-compendium.herokuapp.com/api/v3/compendium/category/monsters');
+            const myJsonData = await data.json();
+            for(var i = 0; i < myJsonData.data.length; i++){
+                var obj = myJsonData.data[i];
+                displayTypeCard(obj);
+            }
+            document.getElementById('main').scrollTop = 0;
+        };
+        equipmentRequest();
+    }else{
+        fadeMain();
+        removeCardsByCategory("monsters");
+    }
+});
+
+
 
 document.getElementById("savedCards").addEventListener("click", function(){
     fadeMain();
@@ -32,11 +114,11 @@ function fadeMain(){
 
 
 const showSavedCards = (obj) => {
+    uncheckAllCheckboxes();
     removeCurrentCards();
-    for(var i = 0; i < obj.data.length; i++){
-        var object = obj.data[i];
-        displayTypeCard(object);
-    }
+    const savedCardData = getSavedCardData();
+    console.log(savedCardData);
+    displayTypeCard(savedCardData);
 }
 
 function removeCurrentCards(){
@@ -46,6 +128,16 @@ function removeCurrentCards(){
     }
 } 
 
+function removeCardsByCategory(category) {
+    const cards = document.querySelectorAll('.card');
+    cards.forEach(card => {
+        const cardCategory = card.querySelector('#cardCategory').innerText.toLowerCase();
+        console.log(cardCategory);
+        if (cardCategory.includes(category.toLowerCase())) {
+            card.remove();
+        }
+    });
+}
 
 window.addEventListener('scroll', this.handleScroll, true);
 
@@ -69,50 +161,82 @@ const cardMachine = (obj) => {
             <div class="card-subtitle text-gray">${obj.id}</div>
         </div>
         <div class="card-body">
+            <div id="cardCategory" class="card-subtitle">Category: ${capitalizedCategory}</div>
+        </div>
+        <div class="card-footer">
+            <i class="icon icon-bookmark saveIcon"></i>
+        </div>
+    </div>`;
+
+
+    return makeCard;
+
+    
+}
+
+const cardFactory = (obj) => {
+    let capitalizedCategory = obj.category.charAt(0).toUpperCase() + obj.category.substring(1);
+    let capitalizedName = obj.name.charAt(0).toUpperCase() + obj.name.substring(1);
+    const makeCard = 
+    `<div class="card">
+        <div class="card-image">
+            <img src="${obj.image}" class="img-responsive">
+            <i class="icon icon-cross" id="removeCross" onClick="this.parentElement.parentElement.remove();"></i>
+        </div>
+        <div class="card-header">
+            <div class="card-title h5">${capitalizedName}</div>
+            <div class="card-subtitle text-gray">${obj.id}</div>
+        </div>
+        <div class="card-body">
             <div class="card-subtitle">Category: ${capitalizedCategory}</div>
         </div>
         <div class="card-footer">
-            <i class="icon icon-plus"></i>
             <i class="icon icon-bookmark"></i>
         </div>
     </div>`;
     return makeCard;
 }
 
-const cardFactory = (obj) => {
-    let capitalizedCategory = obj.data.category.charAt(0).toUpperCase() + obj.data.category.substring(1);
-    let capitalizedName = obj.data.name.charAt(0).toUpperCase() + obj.data.name.substring(1);
-    const makeCard = 
-    `<div class="card">
-        <div class="card-image">
-            <img src="${obj.data.image}" class="img-responsive">
-            <i class="icon icon-cross" id="removeCross" onClick="this.parentElement.parentElement.remove();"></i>
-        </div>
-        <div class="card-header">
-            <div class="card-title h5">${capitalizedName}</div>
-            <div class="card-subtitle text-gray">${obj.data.id}</div>
-        </div>
-        <div class="card-body">
-            <div class="card-subtitle">Category: ${capitalizedCategory}</div>
-        </div>
-        <div class="card-footer">
-            <i class="icon icon-plus"></i>
-            <i class="icon icon-bookmark"></i>
-        </div>
-    </div>`;
-    return makeCard;
+document.addEventListener('click', function(event) {
+    if (event.target && event.target.classList.contains('saveIcon')) {
+        // Get JSON data of the card
+        let cardData = {
+            category: event.target.parentElement.parentElement.querySelector('#cardCategory').innerText,
+            name: event.target.parentElement.parentElement.querySelector('.card-title').innerText,
+            id: event.target.parentElement.parentElement.querySelector('.card-subtitle').innerText,
+            image: event.target.parentElement.parentElement.querySelector('.card-image img').src
+        };
+        console.log(cardData);
+        // Save cardData to local storage
+        let savedCardData = getSavedCardData() || [];
+
+        let exists = savedCardData.findIndex(item => item.id === cardData.id) !== -1;
+
+        if (!exists) {
+            // If the card data doesn't exist, add it to savedCardData
+            savedCardData.push(cardData);
+
+            // Update localStorage with the new savedCardData
+            localStorage.setItem('savedCard', JSON.stringify(savedCardData));
+
+            // Update the UI to display the saved card
+        }
+    }
+});
+
+function getSavedCardData() {
+    const savedCardData = localStorage.getItem('savedCard');
+    return savedCardData ? JSON.parse(savedCardData) : [];
 }
 
 
 function displayCard(obj) {
     let parentNode = document.getElementById('main');
     console.log(parentNode);
-    parentNode.insertAdjacentHTML('beforeend', cardFactory(obj));
+    parentNode.insertAdjacentHTML('afterbegin', cardFactory(obj));
 }
 function displayTypeCard(obj) {
     let parentNode = document.getElementById('main');
     console.log(parentNode);
-    parentNode.insertAdjacentHTML('beforeend', cardMachine(obj));
+    parentNode.insertAdjacentHTML('afterbegin', cardMachine(obj));
 }
-
-testRequest();
